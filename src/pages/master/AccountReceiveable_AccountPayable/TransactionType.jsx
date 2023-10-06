e47fa57be9af73735b4c6ab97a5909bd11cfb550
tenvy <riskypabji@gmail.com>
2023-10-06 13:11:26 +0700

    add: transaction type

--- src/pages/master/AccountReceiveable_AccountPayable/TransactionType.jsx
+++ src/pages/master/AccountReceiveable_AccountPayable/TransactionType.jsx
@@ -8,21 +8,33 @@ import "react-toastify/dist/ReactToastify.css";
 export const TransactionType = () => {
   const { fetchMe, response } = useMe();
   const [code, setCode] = useState("");
-  const [name, setName] = useState("");
-  const [address, setAddress] = useState("");
-  const [city, setCity] = useState("");
-  const [phone, setPhone] = useState("");
+  const [type, setType] = useState("");
+  const [description, setDescription] = useState("");
+  const [accountNo, setAccountNo] = useState();
+  const [purpose, setPurpose] = useState("");
   const [mobile, setMobile] = useState("");
   const [getData, setGetData] = useState([]);
+  const [getMyAccount, setGetAccount] = useState([]);
+
+  const purposeVal = ['PURCHASE COST','ADJUSTMENT IN','ADJUSTMENT OUT','SUPPLIER','CUSTOMER','MATERIAL','CASHIER','PAYMENT','DEBIT/CREDIT NOTE', 'ASSET']
 
   useEffect(() => {
     fetchMe();
   }, [!response]);
 
+  const getAccount = async () => {
+    try {
+      const response = await axios.get(`${process.env.REACT_APP_API_BASE_URL}/account`)
+      setGetAccount(response.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   const dataFetching = async () => {
     try {
       const res = await axios.get(
-        `${process.env.REACT_APP_API_BASE_URL}/approval`
+        `${process.env.REACT_APP_API_BASE_URL}/transactiontype`
       );
       setGetData(res.data);
     } catch (error) {
@@ -79,13 +91,11 @@ export const TransactionType = () => {
   const submitClick = async (e) => {
     e.preventDefault();
     try {
-      await axios.post(`${process.env.REACT_APP_API_BASE_URL}/approval`, {
-        code: code,
-        name: name,
-        address: address,
-        city: city,
-        phone: phone,
-        mobile: mobile,
+      await axios.post(`${process.env.REACT_APP_API_BASE_URL}/transactiontype`, {
+        type: type,
+        description: description,
+        accountNo: accountNo,
+        purpose: purpose,
         createdBy: response.User,
         changedBy: response.User,
       });
@@ -117,7 +127,7 @@ export const TransactionType = () => {
 
   useEffect(() => {
     dataFetching();
-    console.log(getData);
+    getAccount()
   }, []);
 
   return (
@@ -125,47 +135,53 @@ export const TransactionType = () => {
       <div className="text-2xl font-bold mb-4">TransactionType</div>
       <form onSubmit={submitClick}>
         <table className="border-separate border-spacing-2 w-1/2">
-          
           <tr>
             <td className="text-right">Type: </td>
             <td>
-            <input
-            type="text"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder=""
-            value="0"
-            required
-            // disabled
-          />
+              <input
+                type="text"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder=""
+                required
+                onChange={(e)=> setType(e.target.value)}
+              />
             </td>
           </tr>
           <tr>
             <td className="text-right">Description: </td>
             <td>
-            <input
-            type="text"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder=""
-            value="0"
-            required
-            // disabled
-          />
+              <input
+                type="text"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder=""
+                required
+                onChange={(e)=> setDescription(e.target.value)}
+              />
             </td>
           </tr>
           <tr>
-                <td className="text-right">Account no: </td>
-                <td>
-                  <select> 
-                  </select>
-                </td>
-              </tr>
-              <tr>
+            <td className="text-right">Account no: </td>
+            <td>
+              <select onChange={(e)=> setAccountNo(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[30%] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <option disabled selected hidden>Pilih Account</option>
+                {getMyAccount.map((data)=>{
+                  return (
+                    <option value={data.AccountNo}>{data.AccountNo}</option>
+                  )
+                })}
+              </select>
+            </td>
+          </tr>
+          <tr>
             <td className="text-right">Purpose: </td>
             <td>
-              <select className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[30%] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                <option disabled selected hidden>
-                </option>
-                <option></option>
+              <select onChange={(e)=> setPurpose(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[30%] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <option disabled selected hidden></option>
+                {purposeVal.map((data)=>{
+                  return (
+                    <option value={data}>{data}</option>
+                  )
+                })}
               </select>
             </td>
           </tr>
@@ -188,22 +204,16 @@ export const TransactionType = () => {
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
               <th scope="col" className="px-6 py-3">
-                Code
+                Type
               </th>
               <th scope="col" className="px-6 py-3">
-                Name
+                Description
               </th>
               <th scope="col" className="px-6 py-3">
-                Address
+                Account No
               </th>
               <th scope="col" className="px-6 py-3">
-                City
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Phone
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Mobile
+                Purpose
               </th>
               <th scope="col" className="px-6 py-3">
                 Created By
@@ -217,12 +227,32 @@ export const TransactionType = () => {
               <th scope="col" className="px-6 py-3">
                 Changed Date
               </th>
-              <th scope="col" className="px-6 py-3">
-                Control
-              </th>
             </tr>
           </thead>
-         
+          <tbody>
+            {getData.map((res,key)=> {
+              return (
+                <tr
+                  key={key}
+                  className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+                >
+                  <th
+                    scope="row"
+                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    {res.Type}
+                  </th>
+                  <td className="px-6 py-4">{res.Description}</td>
+                  <td className="px-6 py-4">{res.AccountNo}</td>
+                  <td className="px-6 py-4">{res.Purpose}</td>
+                  <td className="px-6 py-4">{res.CreatedBy}</td>
+                  <td className="px-6 py-4">{dateConverter(res.createdDate)}</td>
+                  <td className="px-6 py-4">{res.ChangedBy}</td>
+                  <td className="px-6 py-4">{dateConverter(res.changedDate)}</td>
+                </tr>
+              )
+            })}
+          </tbody>
         </table>
         <div></div>
       </div>
